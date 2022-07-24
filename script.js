d8b29406ea0d950413ec492a349808d7c5cb610c
rckfce <rckfce@gmail.com>
2022-07-25 00:18:39 +0100

    added hover effect with css and js

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 const containerDiv = document.getElementById("grid");
 const viewPointWidth = document.documentElement.clientWidth;
 
+
 let gridSize = 16;
 let gridBoxSize = (viewPointWidth - (gridSize + 1) * 5 - 10) / gridSize;
 
-function drawGrid(size) {
-    for (let j = 1; j <= size; j++) {
-        for (let i = 1; i <= size; i++) {
-            let newDiv = document.createElement("div");
-            newDiv.innerHTML = i;
-            newDiv.classList.add("gridBox");
-            newDiv.style.height = gridBoxSize + "px";
-            newDiv.style.width = gridBoxSize + "px";
-            containerDiv.appendChild(newDiv);
-        }
-    }
+for (let i = 1; i <= gridSize * gridSize; i++) {
+    let newDiv = document.createElement("div");
+    newDiv.innerHTML = i;
+    newDiv.classList.add("gridBox");
+    newDiv.style.height = gridBoxSize + "px";
+    newDiv.style.width = gridBoxSize + "px";
+    containerDiv.appendChild(newDiv);
 }
 
+const box = document.querySelectorAll(".gridBox");
 
-drawGrid(gridSize);
+/*  Doing this with CSS
+box.forEach ((each) => {
+    each.addEventListener("mouseover", event => each.classList.add("onHover"));
+    each.addEventListener("mouseout", event => each.classList.remove("onHover"));
+});
+ */
