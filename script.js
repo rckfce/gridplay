8e2b1b907792f3416a1735923e6bc58bf1cecfb4
rckfce <rckfce@gmail.com>
2022-07-25 19:25:56 +0100

    everything is finished except mouse trail

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const viewPointWidth = document.documentElement.clientWidth;
 let randomColor = "";
 
 let gridSize = 16;
-let gridBoxSize = (viewPointWidth - (gridSize + 1) * 5 - 10) / gridSize;
+let viewNoGaps = viewPointWidth - (gridSize + 1) * 5 - 10;
+let gridBoxSize = (viewNoGaps / gridSize).toFixed(0) - 1;
 
-for (let i = 1; i <= gridSize * gridSize; i++) {
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+    gridSize = Number(prompt("Enter grid size", "16"));
+    if (!gridSize) {window.location.reload(true);}
+    let oldGrid = document.getElementById("grid");
+    while (oldGrid.firstChild) {
+        oldGrid.removeChild(oldGrid.firstChild);
+    }
+    viewNoGaps = viewPointWidth - (gridSize + 1) * 5 - 10;
+    gridBoxSize = (viewNoGaps / gridSize).toFixed(0) - 1;
+    drawGrid();
+    hover();
+});
+
+for (let i = 1; i <= 16 * 16; i++) {
     let newDiv = document.createElement("div");
-    newDiv.innerHTML = i;
     newDiv.classList.add("gridBox");
     newDiv.style.height = gridBoxSize + "px";
     newDiv.style.width = gridBoxSize + "px";
@@ -15,7 +29,18 @@ for (let i = 1; i <= gridSize * gridSize; i++) {
     containerDiv.appendChild(newDiv);
 }
 
-const box = document.querySelectorAll(".gridBox");
+hover();
+
+function drawGrid() {
+    for (let i = 1; i <= gridSize * gridSize; i++) {
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("gridBox");
+        newDiv.style.height = gridBoxSize + "px";
+        newDiv.style.width = gridBoxSize + "px";
+        newDiv.style.backgroundColor
+        containerDiv.appendChild(newDiv);
+    }
+}
 
 function rgb() {
     const random = Math.floor(Math.random() * 8);
@@ -44,9 +69,11 @@ function rgb() {
     }
 }
 
-box.forEach ((each) => {
-    rgb();
-    each.addEventListener("mouseover", event => {rgb(); each.classList.add(randomColor);});
-    each.addEventListener("mouseout", event => each.classList.remove(randomColor));
-});
-
+function hover() {
+    const box = document.querySelectorAll(".gridBox");
+    box.forEach ((each) => {
+        rgb();
+        each.addEventListener("mouseover", event => {rgb(); each.classList.add(randomColor);});
+        each.addEventListener("mouseout", event => each.classList.remove(randomColor));
+    });
+}
