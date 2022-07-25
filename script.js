acdd253921a5cee9f13ef563378808c117ce06b5
rckfce <rckfce@gmail.com>
2022-07-25 17:13:19 +0100

    added rgb hover function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const containerDiv = document.getElementById("grid");
 const viewPointWidth = document.documentElement.clientWidth;
-
+let randomColor = "";
 
 let gridSize = 16;
 let gridBoxSize = (viewPointWidth - (gridSize + 1) * 5 - 10) / gridSize;
@@ -11,14 +11,42 @@ for (let i = 1; i <= gridSize * gridSize; i++) {
     newDiv.classList.add("gridBox");
     newDiv.style.height = gridBoxSize + "px";
     newDiv.style.width = gridBoxSize + "px";
+    newDiv.style.backgroundColor
     containerDiv.appendChild(newDiv);
 }
 
 const box = document.querySelectorAll(".gridBox");
 
-/*  Doing this with CSS
+function rgb() {
+    const random = Math.floor(Math.random() * 8);
+    switch (random) {
+        case 0:
+            randomColor = "red";
+            break;
+        case 1:
+            randomColor = "orange";
+            break;
+        case 3:
+            randomColor = "yellow";
+            break;
+        case 4:
+            randomColor = "green";
+            break;
+        case 5:
+            randomColor = "blue";
+            break;
+        case 6:
+            randomColor = "indigo";
+            break;
+        case 7:
+            randomColor = "violet";
+            break;
+    }
+}
+
 box.forEach ((each) => {
-    each.addEventListener("mouseover", event => each.classList.add("onHover"));
-    each.addEventListener("mouseout", event => each.classList.remove("onHover"));
+    rgb();
+    each.addEventListener("mouseover", event => {rgb(); each.classList.add(randomColor);});
+    each.addEventListener("mouseout", event => each.classList.remove(randomColor));
 });
- */
+
